7121c37c453d2a1243551f1a1f0ee621ac99d232
Jamie Matthews <41803205+Jamium@users.noreply.github.com>
2021-02-01 20:49:25 +0000

    Update docusaurus.config.js

--- ToggleDocs/docusaurus.config.js
+++ ToggleDocs/docusaurus.config.js
@@ -2,7 +2,7 @@ module.exports = {
   title: 'My Site',
   tagline: 'The tagline of my site',
   url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
+  baseUrl: '/ToggleDocs/build/index/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
